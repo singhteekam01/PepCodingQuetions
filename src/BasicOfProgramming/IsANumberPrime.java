81bd9128c6c7c00043ace6f05ba418ee00e69cb8
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 07:53:23 +0530

    IsANumberPrime

---
+++ src/BasicOfProgramming/IsANumberPrime.java
@@ -0,0 +1,35 @@
+package BasicOfProgramming;
+
+import java.util.*;
+
+public class IsANumberPrime {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int t = scn.nextInt();
+
+
+
+        while (t > 0) {
+
+            int n = scn.nextInt();
+
+            boolean isPrime =true;
+
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
+            }
+            t--;
+        }
+
+    }
+}
---
+++ src/BasicOfProgramming/PrintZ.java
@@ -0,0 +1,13 @@
+package BasicOfProgramming;
+
+public class PrintZ {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+    }
+}
