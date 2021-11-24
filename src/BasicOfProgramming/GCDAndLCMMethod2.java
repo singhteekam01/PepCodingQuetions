51e2ff226f98f4fb75e10498ed4953500b2e920b
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 11:07:03 +0530

    GCD and LCM Method 2

---
+++ src/BasicOfProgramming/GCDAndLCMMethod2.java
@@ -0,0 +1,34 @@
+package BasicOfProgramming;
+import java.util.Scanner;
+
+public class GCDAndLCMMethod2 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int num1 = scn.nextInt();
+        int num2 = scn.nextInt();
+
+        int n1 = num1;
+        int n2 = num2;
+
+        //euclid method
+        if(num2 > num1){
+            num1 = num1+num2;
+            num2 = num1-num2;
+            num1 = num1- num2;
+        }
+
+        while(num2 >0){
+            int temp = num2;
+            num2 = num1%num2;
+            num1 = temp;
+        }
+        System.out.println(num1);
+
+        //Lcm of two numbers
+
+        int lcm = n1*n2/num1;
+        System.out.println(lcm);
+
+    }
+}
