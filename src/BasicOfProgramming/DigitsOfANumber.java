35c396f955c545dcbfc0a2585477afeb9f05552d
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 09:39:12 +0530

    Digits Of A Number

---
+++ src/BasicOfProgramming/DigitsOfANumber.java
@@ -0,0 +1,35 @@
+package BasicOfProgramming;
+
+import java.util.Scanner;
+
+public class DigitsOfANumber {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int count =0;
+        int temp = n;
+        while(temp > 0){
+            temp /= 10;
+            count++;
+        }
+
+        int power =1;
+        int tempCount = count;
+        while(tempCount-1 >0){
+            power *= 10;
+            tempCount--;
+        }
+
+        while(count > 0){
+
+            int firstDigit = n/power;
+            n %= power;
+            System.out.println(firstDigit);
+            power /= 10;
+            count--;
+        }
+    }
+}
