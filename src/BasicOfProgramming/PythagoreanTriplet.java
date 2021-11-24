5cfbf2ae783861ade74c0951fed60565290272a4
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 14:16:42 +0530

    Pythagorean Triplet

---
+++ src/BasicOfProgramming/PythagoreanTriplet.java
@@ -0,0 +1,30 @@
+package BasicOfProgramming;
+
+import java.util.Scanner;
+
+public class PythagoreanTriplet {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+
+        if (b > a && b > c) {
+            int temp = b;
+            b = a;
+            b = temp;
+        } else if (c > a && c > b) {
+            int temp = c;
+            c = a;
+            a = temp;
+        }
+
+        if (a * a == (b * b + c * c)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
+}
