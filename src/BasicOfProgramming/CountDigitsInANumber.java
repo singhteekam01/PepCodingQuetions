a658f3fef2c8984451ad9c01a0f0ea3c210cc46f
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 09:28:43 +0530

    Count Digits In A Number

---
+++ src/BasicOfProgramming/CountDigitsInANumber.java
@@ -0,0 +1,22 @@
+package BasicOfProgramming;
+
+import java.util.Scanner;
+
+public class CountDigitsInANumber {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int count =0;
+
+        while(n >0){
+            n /= 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
