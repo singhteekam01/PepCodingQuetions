fb931eba5311d182725fec184327a49f6d8a820b
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 10:05:27 +0530

    Inverse Of A Number

---
+++ src/BasicOfProgramming/InverseOfANumber.java
@@ -0,0 +1,30 @@
+package BasicOfProgramming;
+
+import java.util.Scanner;
+
+public class InverseOfANumber {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int res = 0;
+        int idx =1;
+        while(n >0){
+            int firstDigit = n%10;
+            n /= 10;
+            res +=idx*power(10,firstDigit);
+            idx++;
+        }
+        System.out.println(res);
+    }
+    public static int power(int base, int num){
+        int ans = 1;
+        while(num-1 >0){
+            ans *= base;
+            num--;
+        }
+        return ans;
+    }
+}
