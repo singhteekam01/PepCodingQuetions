c525ebb0030e0828acf5ebc4022266f80ee42fe1
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-24 14:07:41 +0530

    Prime Factorization Of A Number

---
+++ src/BasicOfProgramming/PrimeFactorizationOfANumber.java
@@ -0,0 +1,18 @@
+package BasicOfProgramming;
+
+import java.util.Scanner;
+
+public class PrimeFactorizationOfANumber {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i=2; i<=n; i++){
+            while(n%i == 0){
+                System.out.print(i+" ");
+                n /= i;
+            }
+        }
+    }
+}
