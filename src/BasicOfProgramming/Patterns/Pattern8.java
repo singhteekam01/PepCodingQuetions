df3db437cadbfb9fc5ca1a3ac2d4cab250596029
Teekam Singh Gurjar <teekamsinghchhawari2@gmail.com>
2021-11-25 15:38:39 +0530

    Pattern 8

---
+++ src/BasicOfProgramming/Patterns/Pattern8.java
@@ -0,0 +1,24 @@
+package BasicOfProgramming.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j == n-i-1){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
